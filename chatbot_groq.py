a39d5a0b26acdc4abdff98729032cd6c77f9a8e4
delta-kairos <deltakairos073@gmail.com>
2025-11-20 01:55:34 -0600

    fix

--- chatbot_groq.py
+++ chatbot_groq.py
@@ -287,7 +287,7 @@ INSTRUCCIONES ESTRICTAS:
                         'content': user_content
                     }
                 ],
-                temperature=0.0,
+                temperature=0.1,
                 max_tokens=2500,
                 stream=True
             )
